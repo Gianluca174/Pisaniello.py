298b0fbf8bf56aa07c7215d3bd2f98cf7e4f57bf
Gianluca174 <75366975+Gianluca174@users.noreply.github.com>
2021-01-02 18:06:17 +0100

    Add files via upload

---
+++ stringhe.py
@@ -0,0 +1,20 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> stringa= str(input("inserisci una stringa"))
+inserisci una stringaarmadio
+>>> i = 0
+>>> while i<len(stringa):
+	print(stringa[i])
+	i = i+1
+
+	
+a
+r
+m
+a
+d
+i
+o
+>>> print(i)
+7
+>>> 
